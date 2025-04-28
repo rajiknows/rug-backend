157c7d6d0e82ec219161d2980054d228c898c2fc
rajiknows <rajeshhjhamain@gmail.com>
2025-04-28 20:29:00 +0530

    final refactorings

--- src/alerts.ts
+++ src/alerts.ts
@@ -2,82 +2,82 @@ import { Context } from "hono";
 import { getPrisma } from "./prisma";
 
 export async function makeAlert(
-  c: Context<{ Bindings: { DATABASE_URL: string } }>,
+    c: Context<{ Bindings: { DATABASE_URL: string } }>,
 ) {
-  // mint is the mint address
-  // type is the type of alert
-  // threshold is the threshold of the alert
-  const { mint, type, comparison, threshold, userEmail } = await c.req.json();
-  const prisma = getPrisma(c.env.DATABASE_URL);
-  // check  is the  user alredy has an alert
-  const existeing_alert = await prisma.alert.findFirst({
-    where: {
-      userEmail,
-    },
-  });
+    // mint is the mint address
+    // type is the type of alert
+    // threshold is the threshold of the alert
+    const { mint, type, comparison, threshold, userEmail } = await c.req.json();
+    const prisma = getPrisma(c.env.DATABASE_URL);
+    // check  is the user alredy has an alert
+    const existeing_alert = await prisma.alert.findFirst({
+        where: {
+            userEmail,
+        },
+    });
 
-  if (existeing_alert) {
-    return c.json({ error: "User already has an alert" }, 400);
-  }
+    if (existeing_alert) {
+        return c.json({ error: "User already has an alert" }, 400);
+    }
 
-  await prisma.alert.create({
-    data: {
-      mint,
-      parameter: type,
-      comparison,
-      userEmail,
-      threshold,
-    },
-  });
+    await prisma.alert.create({
+        data: {
+            mint,
+            parameter: type,
+            comparison,
+            userEmail,
+            threshold,
+        },
+    });
 
-  return c.json({ message: "alert created succesfully" });
+    return c.json({ message: "alert created succesfully" });
 }
 
 export async function getAlerts(
-  c: Context<{ Bindings: { DATABASE_URL: string } }>,
+    c: Context<{ Bindings: { DATABASE_URL: string } }>,
 ) {
-  const prisma = getPrisma(c.env.DATABASE_URL);
-  const alerts = await prisma.alert.findMany();
-  return c.json(alerts);
+    const prisma = getPrisma(c.env.DATABASE_URL);
+    const alerts = await prisma.alert.findMany();
+    return c.json(alerts);
 }
 
 export async function deleteAlert(
-  c: Context<{ Bindings: { DATABASE_URL: string } }>,
+    c: Context<{ Bindings: { DATABASE_URL: string } }>,
 ) {
-  const prisma = getPrisma(c.env.DATABASE_URL);
-  const { userEmail } = await c.req.json();
-  await prisma.alert.delete({ where: { userEmail } });
-  return c.json({ message: "Alert deleted successfully" });
+    const prisma = getPrisma(c.env.DATABASE_URL);
+    const { userEmail } = await c.req.json();
+    await prisma.alert.delete({ where: { userEmail } });
+    return c.json({ message: "Alert deleted successfully" });
 }
 
 export async function updateAlert(
-  c: Context<{ Bindings: { DATABASE_URL: string } }>,
+    c: Context<{ Bindings: { DATABASE_URL: string } }>,
 ) {
-  const prisma = getPrisma(c.env.DATABASE_URL);
-  const { userEmail, mint, type, comparison, threshold } = await c.req.json();
-  await prisma.alert.update({
-    where: { userEmail },
-    data: { mint, parameter: type, comparison, threshold },
-  });
-  return c.json({ message: "Alert updated successfully" });
+    const prisma = getPrisma(c.env.DATABASE_URL);
+    const { userEmail, mint, type, comparison, threshold } = await c.req.json();
+    await prisma.alert.update({
+        where: { userEmail },
+        data: { mint, parameter: type, comparison, threshold },
+    });
+    return c.json({ message: "Alert updated successfully" });
 }
 
 export async function getAlertByUserEmail(
-  c: Context<{ Bindings: { DATABASE_URL: string } }>,
+    c: Context<{ Bindings: { DATABASE_URL: string } }>,
 ) {
-  const prisma = getPrisma(c.env.DATABASE_URL);
-  const { userEmail } = await c.req.json();
-  const alert = await prisma.alert.findFirst({ where: { userEmail } });
-  return c.json(alert);
+    const prisma = getPrisma(c.env.DATABASE_URL);
+    const { userEmail } = await c.req.json();
+    const alert = await prisma.alert.findFirst({ where: { userEmail } });
+    return c.json(alert);
 }
 
 export async function getAlertByMint(
-  c: Context<{ Bindings: { DATABASE_URL: string } }>,
+    c: Context<{ Bindings: { DATABASE_URL: string } }>,
 ) {
-  const prisma = getPrisma(c.env.DATABASE_URL);
-  const { mint } = await c.req.json();
-  const alert = await prisma.alert.findFirst({ where: { mint } });
-  return c.json(alert);
+    const prisma = getPrisma(c.env.DATABASE_URL);
+    const { mint } = await c.req.json();
+    const alert = await prisma.alert.findFirst({ where: { mint } });
+    return c.json(alert);
 }
 
 // model Alert {
@@ -97,4 +97,3 @@ export async function getAlertByMint(
 //     // Index to efficiently find active, untriggered alerts for a specific mint and parameter
 //     @@index([mint, parameter, isActive, triggeredAt])
 // }
-
